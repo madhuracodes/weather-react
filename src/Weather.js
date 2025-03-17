cd319869fc7671251eadea1c53ab1187358397cd
madhuracodes <madhura84@gmail.com>
2025-03-17 11:34:11 +0100

    uninstalled loader

--- src/Weather.js
+++ src/Weather.js
@@ -1,17 +1,6 @@
 import react from "react";
-import axios from "axios";
-import { Audio } from "react-loader-spinner";
 
 export default function Weather() {
-  return (
-    <Audio
-      height="80"
-      width="80"
-      radius="9"
-      color="red"
-      ariaLabel="loading"
-      wrapperStyle
-      wrapperClass
-    />
-  );
+  return;
+  <h2>Weather</h2>;
 }
