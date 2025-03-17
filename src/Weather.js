7c67f5d71613c95d7b7a29c469715875d3a6dff4
madhuracodes <madhura84@gmail.com>
2025-03-17 11:20:07 +0100

    added a loader and changed color

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,17 @@
 import react from "react";
 import axios from "axios";
+import { Audio } from "react-loader-spinner";
 
 export default function Weather() {
-  return <h1>Hello from Weather</h1>;
+  return (
+    <Audio
+      height="80"
+      width="80"
+      radius="9"
+      color="red"
+      ariaLabel="loading"
+      wrapperStyle
+      wrapperClass
+    />
+  );
 }
